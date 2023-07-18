8920fd75403f614485f38570b7255a219faac592
pranavhunagund1993 <pranavhunagund788@gmail.com>
2023-07-18 16:51:06 +0530

    react portfolio menu completed

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ import Works from './components/works/Works';
 import Testimonials from './components/testimonials/Testimonials';
 import Contact from './components/contact/Contact';
 import "./app.scss";
+import Menu from './components/menu/Menu';
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false)
   return (
     <div className="app">
       <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className="sections">
         <Intro />
         <Portfolio />
