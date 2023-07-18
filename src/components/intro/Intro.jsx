80aac4bb9eef6159d8587dd0934fe816801c9a88
pranavhunagund1993 <pranavhunagund788@gmail.com>
2023-07-18 18:29:05 +0530

    react portfolio intro completed

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import "./intro.scss";
+import { init } from 'ityped';
 
 function Intro() {
+
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current, { 
+            showCursor: false, 
+            backDelay: 1500,
+            strings: ["Web Developer", "React Developer", "MERN Full Stack Developer"],
+         })
+    }, []);
+
     return (
         <div className="intro" id='intro'>
             <div className="left">
@@ -13,7 +25,7 @@ function Intro() {
                 <div className="wrapper">
                     <h2>Hi There, I'm</h2>
                     <h1>Pranav P Hunagund</h1>
-                    <h3>Freelance <span></span></h3>
+                    <h3><span ref={textRef}></span></h3>
                 </div>
                 <a href="#portfolio">
                     <img src="assets/down.png" alt="Down Arrow" />
