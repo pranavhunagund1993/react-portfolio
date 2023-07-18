1bbf5de5678039c43da053053f5ca7afe91894b7
pranavhunagund1993 <pranavhunagund788@gmail.com>
2023-07-18 18:04:22 +0530

    react portfolio intro updated

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -4,7 +4,21 @@ import "./intro.scss";
 function Intro() {
     return (
         <div className="intro" id='intro'>
-            
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/man.png" alt="Profile" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Pranav P Hunagund</h1>
+                    <h3>Freelance <span></span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/down.png" alt="Down Arrow" />
+                </a>
+            </div>
         </div>
     )
 }
