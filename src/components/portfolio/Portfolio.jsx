2eaf950b34d228f59e6b36a1566d01d8dbe39d63
pranavhunagund1993 <pranavhunagund788@gmail.com>
2023-07-19 12:20:13 +0530

    react portfolio portfolio started

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,7 +4,20 @@ import './portfolio.scss';
 function Portfolio() {
     return (
         <div className="portfolio" id='portfolio'>
-            
+            <h1>Portfolio</h1>
+            <ul>
+                <li>Featured</li>
+                <li>Web App</li>
+                <li>Mobile App</li>
+                <li>Design</li>
+                <li>Branding</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="assets/LinkedIn.jpg" alt="LinkedIn" />
+                    <h3>LinkedIn Clone App</h3>
+                </div>
+            </div>
         </div>
     )
 }
