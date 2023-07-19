ce4d4d824f457cee1673c1ce4f1377eb6f2afcc1
pranavhunagund1993 <pranavhunagund788@gmail.com>
2023-07-19 14:20:21 +0530

    react portfolio list completed

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,18 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './portfolio.scss';
+import PortfolioList from './../portfolioList/portfolioList';
 
 function Portfolio() {
+
+    const [selected, setSelected] = useState("featured");
+
+    const list = [
+        {
+            id: "featured",
+            title: "Featured"
+        },
+        {
+            id: "web",
+            title: "Web App"
+        },
+        {
+            id: "mobile",
+            title: "Mobile App"
+        },
+        {
+            id: "design",
+            title: "Design"
+        },
+        {
+            id: "branding",
+            title: "Branding"
+        }
+    ];
+
     return (
         <div className="portfolio" id='portfolio'>
             <h1>Portfolio</h1>
             <ul>
-                <li>Featured</li>
-                <li>Web App</li>
-                <li>Mobile App</li>
-                <li>Design</li>
-                <li>Branding</li>
+                { list.map((item) => (
+                    <PortfolioList 
+                        title={item.title} 
+                        active={selected === item.id} 
+                        setSelected={setSelected} 
+                        id={item.id}
+                    />
+                ))}
             </ul>
             <div className="container">
+                <div className="item">
+                    <img src="assets/LinkedIn.jpg" alt="LinkedIn" />
+                    <h3>LinkedIn Clone App</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/LinkedIn.jpg" alt="LinkedIn" />
+                    <h3>LinkedIn Clone App</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/LinkedIn.jpg" alt="LinkedIn" />
+                    <h3>LinkedIn Clone App</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/LinkedIn.jpg" alt="LinkedIn" />
+                    <h3>LinkedIn Clone App</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/LinkedIn.jpg" alt="LinkedIn" />
+                    <h3>LinkedIn Clone App</h3>
+                </div>
                 <div className="item">
                     <img src="assets/LinkedIn.jpg" alt="LinkedIn" />
                     <h3>LinkedIn Clone App</h3>
