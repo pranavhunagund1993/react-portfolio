5f3419bbebe4a94b2dd46f673726a61702a7831f
pranavhunagund1993 <pranavhunagund788@gmail.com>
2023-07-19 17:36:07 +0530

    react portfolio works onprogress

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -12,15 +12,19 @@ function Works() {
                                 <div className="imgContainer">
                                     <img src="assets/mobile.png" alt="mobile" />
                                 </div>
-                                <h2>Title</h2>
+                                <h2>LinkedIn Clone</h2>
                                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed aliquid est porro culpa, accusantium consectetur unde esse exercitationem nisi nihil!</p>
                                 <span>Projects</span>
                             </div>
                         </div>
-                        <div className="right"></div>
+                        <div className="right">
+                            <img src="assets/LinkedIn.jpg" alt="LinkedIn" />
+                        </div>
                     </div>
                 </div>
             </div>
+            <img src="assets/arrow.png" className='arrow left' />
+            <img src="assets/arrow.png" className='arrow right' />
         </div>
     )
 }
