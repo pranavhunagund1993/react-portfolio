41865a7af9cefbe3f8f0791c8ea8f2be5c579418
pranavhunagund1993 <pranavhunagund788@gmail.com>
2023-07-19 17:02:47 +0530

    react portfolio works started

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -4,7 +4,23 @@ import './works.scss';
 function Works() {
     return (
         <div className="works" id='works'>
-            
+            <div className="slider">
+                <div className="container">
+                    <div className="item">
+                        <div className="left">
+                            <div className="leftContainer">
+                                <div className="imgContainer">
+                                    <img src="assets/mobile.png" alt="mobile" />
+                                </div>
+                                <h2>Title</h2>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed aliquid est porro culpa, accusantium consectetur unde esse exercitationem nisi nihil!</p>
+                                <span>Projects</span>
+                            </div>
+                        </div>
+                        <div className="right"></div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
